28f961f299c60a8ca155e26d1f4135a88f8d8ace
Kaungmyatkyaw2 <kaungmyatkyaw2030@gmail.com>
2023-10-03 14:10:25 +0630

    fix : package.json

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config({
-  path: './config.env',
+  path: "./config.env",
 });
 
-process.on('uncaughtException', (error) => {
-  (error.message, error.name);
-  console.log('Uncaught Exception Alert!');
+process.on("uncaughtException", (error) => {
+  console.log(err.message, err.name);
+  console.log("Uncaught Exception Alert!");
   process.exit(1);
 });
 
-const app = require('./app');
-const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+const app = require("./app");
+const DB = process.env.DB.replace("<PASSWORD>", process.env.DB_PASSWORD);
 mongoose
   .connect(DB)
   .then((con) => {
-    console.log('SUCCESSFULLY CONNECT');
+    console.log("SUCCESSFULLY CONNECT");
   })
   .catch((err) => console.log(err));
 
 const server = app.listen(process.env.PORT);
 
-process.on('unhandledRejection', (error) => {
+process.on("unhandledRejection", (error) => {
   console.log(err.message, err.name);
-  console.log('Uncaught Handled Rejection ALERT.');
+  console.log("Uncaught Handled Rejection ALERT.");
   server.close(() => {
-    console.error('Server is shutted down.... ! Node process will exit !');
+    console.error("Server is shutted down.... ! Node process will exit !");
     process.exit(1);
   });
 });
